b5bf3adea7619c88bd1831c38caa34c2b004c060
NewgateClocks <mike.harrison@newgateclocks.com>
2015-06-10 12:35:46 +0100

    Added Accordion Module from Refills

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 // @codekit-prepend "../../bower_components/fastclick/lib/fastclick.js";
 
-// FASTCLICK
+// MODULES
+
+// @codekit-prepend "../modules/accordion/accordion.js";
+
+
+//FASTCLICK
+
+
 
 $(function() {
     FastClick.attach(document.body);
