8980f8fd512b0a811aff8d833b22310a57914de6
NewgateClocks <mike.harrison@newgateclocks.com>
2015-06-15 15:26:33 +0100

    Adding horizontal nav

--- src/modules/horizontalnav/horizontalnav.js
+++ src/modules/horizontalnav/horizontalnav.js
@@ -1,16 +1,32 @@
-$(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+$(document).ready(function () {
+    var menuToggle = $('#js-mobile-menu').unbind();
+    
+    $('#js-navigation-menu').removeClass("show");
 
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
+    menuToggle.on('click', function (e) {
+        e.preventDefault();
+        $('#js-navigation-menu').slideToggle("fast", function () {
+            if ($('#js-navigation-menu').is(':hidden')) {
+                $('#js-navigation-menu').removeAttr('style');
+            }
+        });
+    });
+    
+/*   
+
+UNCOMMENT THIS FOR SINGLE-PAGE NAV, SO A CLICK ON A NAV LINK IN DROPDOWN MODE MAKES THE MENU SHRINK UP 
+    
+    var navLink = $('.nav-link').unbind();
+    
+    navLink.on('click', function (e) {
+        e.preventDefault();
+        
+      if ($('#js-mobile-menu').css('display') === 'block') {
+        
+        $('#js-navigation-menu').slideToggle();
+          
       }
     });
-  });
-    
-    //TODO Slide-up javascript for when menu links are clicked in mobile view for single page designs
-    
+*/
+
 });
