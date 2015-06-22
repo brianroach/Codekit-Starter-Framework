2e4b4c1eb70c714ede8448fa1a957ee67374df82
MikeHarrison <mike@mikeharrisondesign.com>
2015-06-22 21:54:24 +0100

    Update horizontalnav.js

--- src/modules/horizontalnav/horizontalnav.js
+++ src/modules/horizontalnav/horizontalnav.js
@@ -1,20 +1,19 @@
-$(document).ready(function () {
-    var menuToggle = $('#js-mobile-menu').unbind();
-    
-    $('#js-navigation-menu').removeClass("show");
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
 
-    menuToggle.on('click', function (e) {
-        e.preventDefault();
-        $('#js-navigation-menu').slideToggle("fast", function () {
-            if ($('#js-navigation-menu').is(':hidden')) {
-                $('#js-navigation-menu').removeAttr('style');
-            }
-        });
+  $('li.nav-link').on('click', function(){
+    if($(".navigation-menu-button").is(":visible")  )  {
+      menuToggle.trigger('click');
+ } 
+  }); 
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
     });
-    
- 
-
-// TODO Javascript for menu to shrink when nav links are clicked, for single-page layouts
-
-
+  });
 });
