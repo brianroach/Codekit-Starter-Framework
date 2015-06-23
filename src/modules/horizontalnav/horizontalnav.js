567d1f1021f36cf1f203cc489a9517e51bb01756
NewgateClocks <mike.harrison@newgateclocks.com>
2015-06-23 14:16:47 +0100

    Comment out menu shrink

--- src/modules/horizontalnav/horizontalnav.js
+++ src/modules/horizontalnav/horizontalnav.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
+    
+/* Uncomment this to make menu shrink on link click, for one-page designs
 
   $('li.nav-link').on('click', function(){
     if($(".navigation-menu-button").is(":visible")  )  {
       menuToggle.trigger('click');
  } 
   }); 
+  
+*/
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
